0c179c8ad010a14f0814c386355793f7d649d857
unknown <12640394+lab127@users.noreply.github.com>
2024-06-27 10:30:43 +0700

    switch expression

--- Program.cs
+++ Program.cs
@@ -1,4 +1,1 @@
-﻿Console.WriteLine(BasicMathOps.BasicOp('+', 4, 7));
-Console.WriteLine(BasicMathOps.TernaryOperator('-', 15, 18));
-Console.WriteLine(BasicMathOps.SwitchExpression('*', 5, 5));
-Console.WriteLine(BasicMathOps.SwitchExpression('/', 49, 7));
+﻿Console.WriteLine(KeepUpTheHook.HoopCount(10));
